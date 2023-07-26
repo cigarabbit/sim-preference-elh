3e45bb90747903b04e128d5839c6467260e13f7f
runchana <runchana.see@outlook.com>
2023-07-26 19:59:48 +0900

    organize BackTraceTable

--- core/src/main/java/io/github/xlives/framework/BackTraceTable.java
+++ core/src/main/java/io/github/xlives/framework/BackTraceTable.java
@@ -1,20 +1,18 @@
 package io.github.xlives.framework;
 
 import io.github.xlives.framework.descriptiontree.Tree;
-import io.github.xlives.framework.descriptiontree.TreeNode;
-import org.apache.commons.io.FileUtils;
 
-import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class BackTraceTable {
-    private Map<Map<Integer, String[]>, List<Map<String, Map<TreeNode<Set<String>>, BigDecimal>>>> backTraceTable = new HashMap<>();
-
     private static Integer index = 0;
-    // debuh
-    public File output_file = new File("/Users/rchn/Desktop/backtracetable");
+
+    private Map<Map<Integer, String[]>, Map<String, Map<Tree<Set<String>>, BigDecimal>>> backTraceTable = new HashMap<>();
+
     public StringBuilder test = new StringBuilder();
 
     private String[] cnPair = new String[2];
@@ -22,34 +20,23 @@ public class BackTraceTable {
     // method to input values for the innermost list of tree nodes
     public void inputTreeNodeValue(Tree<Set<String>> node, BigDecimal values, int order) throws IOException {
 
-        if (output_file.exists()) {
-            output_file.delete();
-        }
+        Map<Tree<Set<String>>, BigDecimal> treeNodeMap = new HashMap<>();
+        Map<String, Map<Tree<Set<String>>, BigDecimal>> innerMap = new HashMap<>();
 
-        Map<TreeNode<Set<String>>, BigDecimal> treeNodeMap = new HashMap<>();
-        Map<String, Map<TreeNode<Set<String>>, BigDecimal>> innerMap = new HashMap<>();
-        List<Map<String, Map<TreeNode<Set<String>>, BigDecimal>>> innerList = new ArrayList<>();
-
-        treeNodeMap.put(node.getNodes().get(0), values);
+        treeNodeMap.put(node, values);
 
         if (order == 1){
             innerMap.put(cnPair[0], treeNodeMap);
         } else {
             innerMap.put(cnPair[1], treeNodeMap);
         }
-        //test.append("=================\n");
 
-        innerList.add(innerMap);
-        // test.append(cnPair[0]).append(cnPair[1]).append("\n").append(innerMap.values()).append("\n");
+        Map<Integer, String[]> outermostMap = setKeyMap(index, cnPair);
 
-        backTraceTable.put(setKeyMap(index, cnPair), innerList);
-
-        test.append(index).append(backTraceTable).append("\n");
-
-        index++;
-
-        FileUtils.writeStringToFile(output_file, test.toString(), true);
-
+        if (!backTraceTable.containsKey(outermostMap)) {
+            backTraceTable.put(outermostMap, innerMap);
+            index++;
+        }
     }
 
     // method to input a key for the outermost HashMap
@@ -64,7 +51,8 @@ public class BackTraceTable {
         return outermostMap;
     }
 
-    public Map<Map<Integer, String[]>, List<Map<String, Map<TreeNode<Set<String>>, BigDecimal>>>> getBackTraceTable() {
+    public Map<Map<Integer, String[]>, Map<String, Map<Tree<Set<String>>, BigDecimal>>> getBackTraceTable() {
         return backTraceTable;
     }
+
 }
