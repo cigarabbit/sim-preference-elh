e96ff57057a65b0011c8db3565b32139a127ff77
runchana <runchana.see@outlook.com>
2023-07-27 12:08:45 +0900

    prevent repeated class

--- core/src/main/java/io/github/xlives/service/OWLClassExtractor.java
+++ core/src/main/java/io/github/xlives/service/OWLClassExtractor.java
@@ -1,15 +1,14 @@
 package io.github.xlives.service;
 
-import org.apache.commons.io.FileUtils;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.util.ShortFormProvider;
-import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+        import org.apache.commons.io.FileUtils;
+        import org.semanticweb.owlapi.apibinding.OWLManager;
+        import org.semanticweb.owlapi.model.*;
+        import org.semanticweb.owlapi.util.ShortFormProvider;
+        import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
-import java.io.File;
-import java.io.IOException;
+        import java.io.File;
+        import java.io.IOException;
 
-// pair a class with ontology
 public class OWLClassExtractor {
     public static void main(String[] args) throws IOException {
         String owlFilePath = "/Users/rchn/Desktop/refactor/sim-preference-elh/batch-owl-topdown-sim/input/output.owl";
@@ -17,6 +16,7 @@ public class OWLClassExtractor {
 
         StringBuilder ResultOutput;
         ResultOutput = new StringBuilder();
+
 
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         OWLOntology ontology;
@@ -28,16 +28,17 @@ public class OWLClassExtractor {
         }
 
         ShortFormProvider shortFormProvider = new SimpleShortFormProvider();
+
         for (OWLClass owlClass1 : ontology.getClassesInSignature()) {
             String className1 = shortFormProvider.getShortForm(owlClass1);
 
             for (OWLClass owlClass2 : ontology.getClassesInSignature()) {
                 String className2 = shortFormProvider.getShortForm(owlClass2);
 
-                if (className1.equals(className2)) {
-                    continue; // skip pairing a class with itself
+                if (className1.equals(className2) || className1.compareTo(className2) >= 0) {
+                    continue; // Skip pairing if className1 is equal to or comes after className2 lexicographically
                 }
-
+                System.out.println(className1 + " " + className2);
                 ResultOutput.append(className1 + " " + className2);
                 ResultOutput.append("\n");
             }
